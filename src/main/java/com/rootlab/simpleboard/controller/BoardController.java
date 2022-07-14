6aaadb1a1f2074a900fe2abb1f0c84ad7a8837fb
Junek3 <rlarudrms28@gmail.com>
2022-07-15 06:37:59 +0900

    JPA를 이용해서 페이징과 검색 처리하기

--- src/main/java/com/rootlab/simpleboard/controller/BoardController.java
+++ src/main/java/com/rootlab/simpleboard/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.rootlab.simpleboard.model.Board;
 import com.rootlab.simpleboard.repository.BoardRepository;
 import com.rootlab.simpleboard.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+
+import static java.lang.Math.max;
 
 @Controller
 @RequestMapping(value = "/board")
@@ -22,8 +28,16 @@ public class BoardController {
 	private BoardValidator boardValidator;
 
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Board> boards = boardRepository.findAll();
+	public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+					   @RequestParam(required = false, defaultValue = "") String searchText) {
+//		List<Board> boards = boardRepository.findAll();
+//		Page<Board> boards = boardRepository.findAll(PageRequest.of(1, 20));
+//		Page<Board> boards = boardRepository.findAll(pageable);
+		Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+		int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
 		model.addAttribute("boards", boards);
 		return "board/list";
 	}
@@ -42,7 +56,7 @@ public class BoardController {
 	@PostMapping("/form")
 	public String formSubmit(@Valid Board board, BindingResult bindingResult) {
 		boardValidator.validate(board, bindingResult);
-		if (bindingResult.hasErrors()){
+		if (bindingResult.hasErrors()) {
 			return "board/form";
 		}
 		System.out.println(board.getId());
